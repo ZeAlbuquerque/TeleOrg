8da73f93bb96ae40a600ef8d106dc7b17c634c14
ZeAlbuquerque <j.vitoralbuquerque@hotmail.com>
2020-10-19 22:04:54 -0300

    Retirando validador das depêndencias

--- src/main/java/br/com/fiap/teleorg/repository/DoadorRepository.java
+++ src/main/java/br/com/fiap/teleorg/repository/DoadorRepository.java
@@ -3,6 +3,15 @@ package br.com.fiap.teleorg.repository;
 
 import br.com.fiap.teleorg.domain.Doador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DoadorRepository extends JpaRepository<Doador, Integer> {
+
+    @Query(value = "select * from doador d where d.nome like '%:nome%' ", nativeQuery = true)
+    List<Doador> findByName(@Param("nome") String nome);
+
+
 }
